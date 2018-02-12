85ca49819bdcd546a1539c278353e59c298d7f18
Anand Sonawane <avsonawane28@gmail.com>
2018-02-12 13:00:13 +0530

    training folder changes

--- training/VOC/GCP/train_2_ps.sh
+++ training/VOC/GCP/train_2_ps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python /home/aodev/keras-retinanet/keras_retinanet/bin/train_pascal.py csv -annotations /home/aodev/keras-retinanet/keras-retinanet/training/VOC/Paperscape/pascalvoc_retinanet_train.csv -classes /home/aodev/keras-retinanet/keras-retinanet/training/VOC/Paperscape/pascalvoc_retinanet_classes.csv -val-annotations /home/aodev/keras-retinanet/keras-retinanet/training/VOC/Paperscape/pascalvoc_retinanet_valid.csv -steps 4000 -batch-size 8
+python /home/aodev/keras-retinanet/keras_retinanet/bin/train_pascal.py csv -annotations /home/aodev/keras-retinanet/keras-retinanet/training/VOC/GCP/pascalvoc_retinanet_train.csv -classes /home/aodev/keras-retinanet/keras-retinanet/training/VOC/GCP/pascalvoc_retinanet_classes.csv -val-annotations /home/aodev/keras-retinanet/keras-retinanet/training/VOC/GCP/pascalvoc_retinanet_valid.csv -steps 16000 -batch-size 2
