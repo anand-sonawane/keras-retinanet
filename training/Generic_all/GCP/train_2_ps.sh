1ffe18fa6e2643b52cb7a0903eff47084988b171
anand-sonawane <avsonawane28@gmail.com>
2018-02-14 09:52:37 +0000

    Changes to bash for new folder

--- training/Generic_all/GCP/train_2_ps.sh
+++ training/Generic_all/GCP/train_2_ps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python /home/aodev/keras-retinanet/keras_retinanet/bin/train_pascal.py --steps 16000 --batch-size 2 csv /home/aodev/keras-retinanet/training/Generic/GCP/pascalvoc_retinanet_train.csv /home/aodev/keras-retinanet/training/Generic/GCP/pascalvoc_retinanet_classes.csv --val-annotations /home/aodev/keras-retinanet/training/Generic/GCP/pascalvoc_retinanet_valid.csv
+python /home/aodev/keras-retinanet/keras_retinanet/bin/train_pascal.py --steps 16000 --batch-size 2 csv /home/aodev/keras-retinanet/training/Generic_all/GCP/pascalvoc_retinanet_train.csv /home/aodev/keras-retinanet/training/Generic_all/GCP/pascalvoc_retinanet_classes.csv --val-annotations /home/aodev/keras-retinanet/training/Generic_all/GCP/pascalvoc_retinanet_valid.csv
 
 
