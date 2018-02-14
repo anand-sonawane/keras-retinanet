6f1e221726397d0cf81d9ecfc0582023402d2e4e
Anand Sonawane <avsonawane28@gmail.com>
2018-02-14 10:46:02 +0530

    Adding a new folder in traning

---
+++ training/Generic_all/GCP/train_4_ps .sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python /home/aodev/keras-retinanet/keras_retinanet/bin/train_pascal.py --steps 8000 --batch-size 4 csv /home/aodev/keras-retinanet/training/Generic/GCP/pascalGeneric_retinanet_train.csv /home/aodev/keras-retinanet/training/Generic/GCP/pascalGeneric_retinanet_classes.csv --val-annotations /home/aodev/keras-retinanet/training/Generic/GCP/pascalGeneric_retinanet_valid.csv
